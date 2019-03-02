d61a093e90cb17d7761be109e6e4feca9eb2ed69
sun <1252430353@qq.com>
2019-03-02 23:59:05 +0800

    第一次发布的内容

---
+++ IMUT_ckEc/src/main/java/cn/java/ckEc/IMUT_ckEc/ImutCkEcApplication.java
@@ -0,0 +1,14 @@
+package cn.java.ckEc.IMUT_ckEc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImutCkEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImutCkEcApplication.class, args);
+	}
+
+}
+
